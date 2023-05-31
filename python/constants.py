0279d86cf6d4a91c776993f4c066e19c0314986c
Suhney23 <73618656+Suhney23@users.noreply.github.com>
2023-05-30 22:24:03 -0400

    poop

--- python/constants.py
+++ python/constants.py
@@ -1,9 +1,4 @@
-import os
-from dotenv import load_dotenv
-
-
 class Constants:
-    load_dotenv('.env')
     url = "https://www.cursemaven.com/test/"
     apiEndpoint = "https://api.curseforge.com"
     version = "1.5.0"
